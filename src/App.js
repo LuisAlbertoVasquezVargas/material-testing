4d45e75429db817813ffa92ceb91b7c9038400b6
Luis Vásquez <you@example.cm>
2018-11-29 19:00:18 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,73 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import Button from "@material/react-button";
+import MaterialIcon from "@material/react-material-icon";
+import { Cell, Grid, Row } from "@material/react-layout-grid";
+import List, {
+  ListItem,
+  ListItemGraphic,
+  ListItemText,
+  ListItemMeta
+} from "@material/react-list";
+
+import "./App.scss";
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div>
+        <List className="mdc-list" avatarList twoLine>
+          <ListItem className="mdc-list-item">
+            <ListItemGraphic
+              className="mdc-list-item__graphic"
+              graphic={
+                <MaterialIcon 
+                  className="mdc-icon-button mdc-button__icon" 
+                  icon="delete_forever" 
+                />
+              }
+            />
+            <ListItemText
+              className="mdc-list-item__text"
+              primaryText="Lorem ipsum culpa consectetur enim do duis in qui sit laborum sunt."
+              secondaryText="Lorem ipsum culpa consectetur enim do duis in qui sit laborum sunt."
+            />
+            <ListItemMeta
+              className="mdc-list-item__meta mdc-list-item__graphic"
+              meta={
+                <MaterialIcon 
+                  className="mdc-icon-button mdc-button__icon" 
+                  icon="delete_forever" 
+                />
+              }
+            />
+          </ListItem>
+          <ListItem className="mdc-list-item">
+            <ListItemGraphic
+              className="mdc-list-item__graphic"
+              graphic={
+                <MaterialIcon 
+                  className="mdc-icon-button" 
+                  icon="delete_forever"
+                />
+              }
+            />
+            <ListItemText
+              className="mdc-list-item__text"
+              primaryText="Lorem ipsum et in ullamco ut labore est sunt nulla aliquip eu adipisicing officia irure ut qui."
+              secondaryText="Lorem ipsum et in ullamco ut labore est sunt nulla aliquip eu adipisicing officia irure ut qui."
+            />
+          </ListItem>
+        </List>
+        <Button
+          className="mdc-button"
+          icon={
+            <MaterialIcon 
+              className="mdc-icon-button" 
+              icon="delete_forever"
+            />
+          }
+        >
+        </Button>
       </div>
     );
   }
